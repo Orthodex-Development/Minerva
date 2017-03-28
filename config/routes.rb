3401be943c099d1981b089444448c6d0a55a41a3
Ishaan Malhi <ishaan.malhi@gmail.com>
2017-03-28 19:52:10 +0530

    feat: Add views to retrieve results from Redis

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'index#index'
+  get '/results/:id' => 'index#show', as: :results
+
   require 'sidekiq/web'
-  
+
   namespace :api, defaults: { format: :json } do
     post '/tokenize', to: 'tokenizer#tokenize'
   end
